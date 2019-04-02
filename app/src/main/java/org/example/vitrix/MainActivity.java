cea6231157f533492e67b52ccfc69ad5f9df3481
Ayush Kumar <ayushkumar0630@gmail.com>
2019-04-02 00:46:20 -0500

    adding animation

--- app/src/main/java/org/example/vitrix/MainActivity.java
+++ app/src/main/java/org/example/vitrix/MainActivity.java
@@ -1,7 +1,9 @@
 package org.example.vitrix;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +11,20 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private final int SETTINGS_INT = 1;
+    private ConstraintLayout mainlayout;
+    private AnimationDrawable dynamicbackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up dynamic background
+        mainlayout = findViewById(R.id.mainlayout);
+        dynamicbackground = (AnimationDrawable) mainlayout.getBackground();
+        dynamicbackground.setEnterFadeDuration(4000);
+        dynamicbackground.setExitFadeDuration(4000);
+        dynamicbackground.start();
 
         Button register = findViewById(R.id.main_register);
         register.setOnClickListener(new View.OnClickListener() {
